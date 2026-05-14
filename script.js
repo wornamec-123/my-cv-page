7a5e5a27196afb276d96333746d33a14fe7b40c8
wornamec-123 <wornamec@gmail.com>
2026-05-14 16:42:52 +0000

    Add button functionality to show message on click

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ let myMajor = "Information Technology";
 // This will create a pop-up on your phone screen
 alert("Hello! My name is " + myName + " and I study " + myMajor);
  
+// 1. Find the button in the HTML 'hardware'
+const btn = document.getElementById("myButton");
+
+// 2. Create the 'Logic' (The Function)
+function showMessage() {
+    alert("Message Sent Successfully!");
+    btn.style.backgroundColor = "green";
+    btn.innerHTML = "Sent!";
+}
+
+// 3. Connect the trigger (The Event Listener)
+btn.addEventListener("click", showMessage);
