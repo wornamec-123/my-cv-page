1f774033a2b4351cbf75e2873387f05441f71976
wornamec-123 <wornamec@gmail.com>
2026-05-14 16:12:42 +0000

    Add variables and console output for user info

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// 1. Create variables
+const myName = "Celestine";
+let myMajor = "Information Technology";
+let level = 100;
+
+// 2. Output the data to the browser's console
+console.log("Hello! My name is " + myName);
+console.log("I am studying " + myMajor + " at Level " + level);
 
