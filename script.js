84373c2ae6bf34b14764a8064171ba52460a48f4
wornamec-123 <wornamec@gmail.com>
2026-05-14 16:16:02 +0000

    Change console logs to alert for user greeting
    Replaced console output with alert for user notification.

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// 1. Create variables
 const myName = "Celestine";
 let myMajor = "Information Technology";
-let level = 100;
 
-// 2. Output the data to the browser's console
-console.log("Hello! My name is " + myName);
-console.log("I am studying " + myMajor + " at Level " + level);
-
+// This will create a pop-up on your phone screen
+alert("Hello! My name is " + myName + " and I study " + myMajor);
+ 
